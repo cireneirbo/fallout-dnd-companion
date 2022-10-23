e2c546f1702b1046ea38818c82509216b002df7a
Eric O'Brien <cireneirbo@gmail.com>
2022-10-23 00:13:22 -0400

    Return a nice array

--- api/dataProcessor.js
+++ api/dataProcessor.js
@@ -1,25 +1,34 @@
 function processDataQuickly () {
-  const rawData = `
+  let rawData = `
     Alien Atomizer	5500			4d6		45/365			2 lb.	Alien Power Module			20		p			3
     Alien Blaster	4000			4d6		30/240			2 lb.	Alien Power Module			10		p			3, 4, 76
     Laser Pistol	65			1d4+1		100/150			3 lb.	Small Energy Cell			30		p			1, 2, Tactics, 3, NV, 4, 76
   `;
   
-  let processedData = rawData.split("\t");
+  rawData = rawData.split("\t").join("\n").split("\n");
+  console.log(rawData);
   const numberOfColumns = 9;
   //console.log(processedData);
   // processedData = processedData.join(",");
   // console.log(processedData);
   // processedData = processedData.split(",");
   // console.log(processedData);
-  let newData = [];
-  for(let i = 0; i < processedData.length; i++) {
-    //if(processedData[i].includes("")) {
-      newData.push(processedData[i].trim());
+  let trimmedData = [];
+  
+  for(let i = 0; i < rawData.length; i++) {
+    
+    //if(!processedData[i].trim().includes('')) {
+      trimmedData.push(rawData[i].trim());
       //console.log(newData)
     //}
   }
-  console.log(newData)
+  console.log(trimmedData);
+  let processedData = trimmedData.filter(function(el) {
+    return el != "";
+  });
+  // processedData = processedData.join("\n").split("\n");
+  console.log(processedData);
+
 }
 
 processDataQuickly();
