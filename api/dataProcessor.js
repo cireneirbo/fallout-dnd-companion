f820c9392d3fed2db9ce09bb5e8a0515cfcd4882
Eric O'Brien <cireneirbo@gmail.com>
2022-10-23 00:25:39 -0400

    Ready to split processed data into sub arrays

--- api/dataProcessor.js
+++ api/dataProcessor.js
@@ -4,30 +4,33 @@ function processDataQuickly () {
     Alien Blaster	4000			4d6		30/240			2 lb.	Alien Power Module			10		p			3, 4, 76
     Laser Pistol	65			1d4+1		100/150			3 lb.	Small Energy Cell			30		p			1, 2, Tactics, 3, NV, 4, 76
   `;
-  
+
+  // remove tabs and newlines and create an array
   rawData = rawData.split("\t").join("\n").split("\n");
-  console.log(rawData);
-  const numberOfColumns = 9;
-  //console.log(processedData);
-  // processedData = processedData.join(",");
-  // console.log(processedData);
-  // processedData = processedData.split(",");
-  // console.log(processedData);
-  let trimmedData = [];
   
+  // the number of columns in the original data to create sub-arrays with
+  const numberOfColumns = 9;
+  
+  // an array to push trimmed elements of rawData to
+  let trimmedData = [];
+
+  // trim the whitespace of each element and push to trimmedData
   for(let i = 0; i < rawData.length; i++) {
-    
-    //if(!processedData[i].trim().includes('')) {
-      trimmedData.push(rawData[i].trim());
-      //console.log(newData)
-    //}
+     
+    trimmedData.push(rawData[i].trim());
+      
   }
-  console.log(trimmedData);
+  
+  // remove the empty elements
   let processedData = trimmedData.filter(function(el) {
     return el != "";
   });
-  // processedData = processedData.join("\n").split("\n");
+
   console.log(processedData);
+
+  for(let i = 0; i < processedData.length; i++) {
+    
+  }
 
 }
 
