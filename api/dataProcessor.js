23aad70bcc334b68af36117697443516981d4127
Eric O'Brien <cireneirbo@gmail.com>
2022-10-27 21:17:54 -0400

    formatting and add fs module

--- api/dataProcessor.js
+++ api/dataProcessor.js
@@ -1,10 +1,14 @@
+const fs = require('fs');
+
+
 let rawDataString = `
     Alien Atomizer	5500			4d6		45/365			2 lb.	Alien Power Module			20		p			3
     Alien Blaster	4000			4d6		30/240			2 lb.	Alien Power Module			10		p			3, 4, 76
     Laser Pistol	65			1d4+1		100/150			3 lb.	Small Energy Cell			30		p			1, 2, Tactics, 3, NV, 4, 76
   `;
 
-  let numberOfColumnsInData = 9;
+let numberOfColumnsInData = 9;
+
 function createArrayFromRawData(rawData) {
 
   // remove tabs and newlines and create an array
@@ -70,7 +74,8 @@ function processDataQuickly () {
   console.log(createArrayFromRawData(rawDataString));
 
   console.log(splitArrayIntoSubArrays(numberOfColumnsInData));
-  
+  outputArray = splitArrayIntoSubArrays(numberOfColumnsInData);
+  fs.appendFileSync('output.js', outputArray);
 
 }
 
